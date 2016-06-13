b03df220d07a8e765c20f37c6ca39ed7dde52985
Scott Cowan <info@scottcowan.org>
2016-06-13 17:18:10 +0100

    Adding Install script to mark config transforms as dependent upon

---
+++ scripts/Install.ps1
@@ -0,0 +1,26 @@
+param($installPath, $toolsPath, $package, $project)
+# Sets the configuration files to have dependent transforms (.Debug/.Release).
+# Selections of items in the project are done with Where-Object rather than direct
+# access into the ProjectItems collection because if the object is moved or doesn't
+# exist then Where-Object will give us a null response rather than the error that
+# DTE will give us.
+
+$baseConfig = $project.ProjectItems | Where-Object { $_.Properties.Item("Filename").Value -eq "App.config" -and $_.ProjectItems.Count -eq 0 }
+if($baseConfig -eq $null)
+{
+  # Upgrade scenario - user has moved/removed the MyConfig.config file
+  # or it already has the dependent items set.
+  return
+}
+
+# Config file exists, so update the properties.
+$baseConfig.Properties.Item("SubType").Value = "Designer"
+
+$debugConfigs = $project.ProjectItems | Where-Object { $_.Properties.Item("Filename").Value -match "^App[.].*[.]config" }
+
+$debugConfigs | ForEach-Object -process {
+	# Handle the update for MyConfig.Debug.config - set it as BuildAction = None
+	# and move it to be a dependency of MyConfig.config.
+	$debugConfig.Properties.Item("ItemType").Value = "None"
+	$baseConfig.ProjectItems.AddFromFile($debugConfig.Properties.Item("FullPath").Value)
+}
